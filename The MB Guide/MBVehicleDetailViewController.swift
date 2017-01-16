b4e5af0485ef9065fc156f82788f93e8c1acb1ca
Martin Zhang <martinzhang@Martins-MacBook-Pro.local>
2017-01-16 15:53:04 -0800

    App icon added.

--- The MB Guide/MBVehicleDetailViewController.swift
+++ The MB Guide/MBVehicleDetailViewController.swift
@@ -20,7 +20,7 @@ class MBVehicleDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        vehicleDetailImageView.image = UIImage(named: vehicleDetailImageName)
+        vehicleDetailImageView.image = UIImage(named: vehicleDetailImageName + "_Detail")
         vehicleDetailTitleLabel.text = vehicleDetailTitleText
         // Do any additional setup after loading the view.
     }
